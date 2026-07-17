854ca73a43550ba9991baa65b1b2d0a63ba16147
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-17 03:50:03 +0200

    Add type hints for Path and Any in problem.py

--- scripts/documentation/problem.py
+++ scripts/documentation/problem.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from typing import Any
 def add_problem(problems: list[dict], file: Path | str, severity: str, message: str):
     problems.append({
         "file": file.as_posix(),
