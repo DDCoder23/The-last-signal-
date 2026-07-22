818348b1e4575513c170fd0c5bc656f3b87b80a5
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-22 09:21:19 +0200

    Add basic TCP client implementation

--- client_python/client.py
+++ client_python/client.py
@@ -1,1 +1,9 @@
+import socket
 
+HOST = "127.0.0.1"
+PORT = 5000
+def main():
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client.connect((HOST, PORT))
+  print("Connecté au serveur")
+  client.close()
