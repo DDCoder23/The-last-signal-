87439b5d2e00ae4811db82b6460a244b611564a1
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-22 09:31:25 +0200

    Refactor test_main to run without CI skip
    Removed skip condition for CI environment in test.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,12 +1,8 @@
 from client_python.main import main
-import os
-import pytest
 
 
-@pytest.mark.skipif(
-    os.environ.get("CI") == "true",
-    reason="Requires graphical environment"
-)
+
+
 def test_main():
     main()
 
