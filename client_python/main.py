b7c0316158790ab5ecb0a9668fda7b904cb3f203
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-23 10:19:23 +0900

    Fix packet sending to encode message properly

--- client_python/main.py
+++ client_python/main.py
@@ -4,6 +4,7 @@ def main():
   client = Client()
   client.connect()
   time.sleep(1)
-  client.send_packet({Bonjour}):
-        
+  client.send_packet(
+        "Bonjour serveur".encode("utf-8")
+  )
   client.disconnect()
