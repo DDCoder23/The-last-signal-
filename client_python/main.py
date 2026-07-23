e2f34f35ecdc532689cffae6666c6b16b2afde19
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-23 18:09:40 +0900

    Refactor packet sending to use specific packet classes

--- client_python/main.py
+++ client_python/main.py
@@ -1,23 +1,18 @@
 from .client import Client
 import time
 from .packets.chat import ChatPacket
+from .packets.login import LoginPacket
+from .packets.ping import PingPacket
+from .packets.move import MovePacket
 def main():
     client = Client()
     client.connect()
     time.sleep(1)
-    client.send_packet(
-    Packet(
-        PacketType.LOGIN,
-        b"Momo"
-    )
-  )
+    client.send_packet(LoginPacket("Momo"))
     response = client.receive_packet()
     print(response.username)
     client.send_packet(
-    Packet(
-        PacketType.CHAT,
-        b"Momo dirige le jeu"
-    )
+        ChatPacket("Momo dirige le jeu")
   )
     response = client.receive_packet()
     print(response.message)
