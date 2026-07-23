dc53729df3d4c713e77fe9144c2cfbd94f6f316d
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-23 16:07:54 +0900

    Add conditional skip for test_main in GitHub workflow
    Skip test_main if running in GitHub workflow for Python.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,8 +1,12 @@
 from client_python.main import main
+import os
+import pytest
 
 
-
-
+@pytest.mark.skipif(
+    os.getenv("GITHUB_WORKFLOW") == "Python",
+    reason="Ignoré dans le workflow Integration Tests"
+)
 def test_main():
     main()
 
