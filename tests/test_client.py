5213dc454ff3c8c4510378cab1dba6f9dd0e6278
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-23 16:21:43 +0900

    Refactor test_main by removing skip condition
    Removed skip condition for test_main function.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,12 +1,5 @@
 from client_python.main import main
-import os
-import pytest
 
-
-@pytest.mark.skipif(
-    os.getenv("GITHUB_WORKFLOW") == "Python CI Report",
-    reason="Ignoré dans le workflow Python car pas de serveur"
-)
 def test_main():
     main()
 
