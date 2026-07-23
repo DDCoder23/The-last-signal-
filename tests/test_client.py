b23ee0bdad1575d967afc4aea9ab5553551136f4
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-23 16:08:55 +0900

    Update test skip condition for Python CI workflow

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,8 +4,9 @@ import pytest
 
 
 @pytest.mark.skipif(
-    os.getenv("GITHUB_WORKFLOW") == "Python",
-    reason="Ignoré dans le workflow Integration Tests"
+    os.getenv("GITHUB_WORKFLOW") == "Python CI Report
+    ",
+    reason="Ignoré dans le workflow Python car pas de serveur"
 )
 def test_main():
     main()
