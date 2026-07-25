bfd8d48ca86646056c93f9fefc758a86fa81bbf2
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-25 11:26:10 +0900

    Add second client instance in main function

--- client_python/main.py
+++ client_python/main.py
@@ -6,8 +6,10 @@ from .packets.ping import PingPacket
 from .packets.move import MovePacket
 def main():
     client = Client()
+    client2 = Client()
     client.connect()
     time.sleep(1)
+    client2.connect()
     client.send_packet(LoginPacket("Momo"))
     response = client.receive_packet()
     print(response.username)
