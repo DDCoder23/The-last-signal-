c81e1cf3162eb8fc8c26662711a601fbbb272743
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-25 14:27:54 +0900

    Refactor get_connection to simplify database access
    Removed the previous implementation of get_connection that included database inspection and logging.

--- client_python/database.py
+++ client_python/database.py
@@ -12,46 +12,8 @@ def get_connection():
     DB_PATH.parent.mkdir(parents=True, exist_ok=True)
     return sqlite3.connect(DB_PATH)
     
-def get_connection():
-    """
-    Retourne une connexion SQLite.
-    """
-    
-    
-    
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    if DB_PATH.exists():
-        print(DB_PATH.stat().st_size)
-        cursor.execute("SELECT COUNT(*) FROM logs")
-        print(cursor.fetchone())
-        cursor.execute("""
-          SELECT timestamp, level, message
-           FROM logs
-           ORDER BY id DESC
-           LIMIT 10""")
-        for row in cursor.fetchall():
-            print(row)
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    tables = cursor.fetchall()
-    print(f"Nombre de tables : {len(tables)}")
-    for (table_name,) in tables:
-        print("\n" + "=" * 60)
-        print(f"TABLE : {table_name}")
-        print("=" * 60)
-        cursor.execute(f"PRAGMA table_info({table_name})")
-        columns = cursor.fetchall()
-        print("Colonnes :")
-        for col in columns:
-            print(col)
-        cursor.execute(f"SELECT * FROM {table_name}")
-        rows = cursor.fetchall()
-        print(f"\nNombre de lignes : {len(rows)}")
-        for row in rows:
-            print(row)
 
 
-    return conn
 
 
 def init_database():
