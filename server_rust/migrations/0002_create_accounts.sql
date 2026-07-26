ee9eb9abe5ebb3708a31e27f2331b07a2dfa4350
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-26 20:58:51 +0900

    Modify accounts table creation to avoid errors

--- server_rust/migrations/0002_create_accounts.sql
+++ server_rust/migrations/0002_create_accounts.sql
@@ -1,12 +1,10 @@
-CREATE TABLE accounts (
+CREATE TABLE IF NOT EXISTS accounts (
 
     account_id BIGSERIAL PRIMARY KEY,
 
     user_id UUID NOT NULL,
 
     username TEXT NOT NULL UNIQUE,
-
-    password_hash TEXT NOT NULL,
 
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
 
