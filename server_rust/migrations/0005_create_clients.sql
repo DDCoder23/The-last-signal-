7481dfcad46d962a9235851627ce6548ff6c27ea
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-26 21:02:30 +0900

    Create clients table in migration 0005

---
+++ server_rust/migrations/0005_create_clients.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS clients (
+
+    client_id BIGSERIAL PRIMARY KEY,
+
+    user_id UUID,
+
+    platform TEXT,
+
+    game_version TEXT,
+
+    os TEXT,
+
+    cpu TEXT,
+
+    gpu TEXT,
+
+    first_seen TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    last_seen TIMESTAMP,
+
+    FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
+        ON DELETE SET NULL
+
+);
