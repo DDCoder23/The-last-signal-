5f80fc70f049f5c4660e6164f04fcb8424bb490d
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-26 22:08:41 +0900

    Convert main to async and add database integration
    Refactor main function to be asynchronous and integrate database connection and migrations.

--- server_rust/src/main.rs
+++ server_rust/src/main.rs
@@ -1,12 +1,46 @@
+mod database;
 mod network;
 
+use database::{
+    database_manager::DatabaseManager,
+    migrations,
+};
 use network::server::Server;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    let server =
-        Server::new("127.0.0.1:5000");
+    println!("==============================");
+    println!("The Last Signal Server");
+    println!("==============================");
 
-    server.start();
+    // URL PostgreSQL
+    let database_url =
+        std::env::var("DATABASE_URL")
+            .expect("DATABASE_URL non définie.");
 
+    // Connexion
+    let database =
+        DatabaseManager::new(&database_url)
+            .await?;
+
+    // Vérifie la connexion
+    database.ping().await?;
+
+    println!("Connexion PostgreSQL établie.");
+
+    // Applique les migrations
+    migrations::run(&database.pool).await?;
+
+    println!("Base de données prête.");
+
+    // Démarrage du serveur
+    let server = Server::new(
+        "127.0.0.1:5000",
+        database,
+    );
+
+    server.start().await;
+
+    Ok(())
 }
