f26a31a6690f980ab8b4d009a9ab228b4cab225c
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-27 16:42:47 +0900

    Update main.rs

--- server_rust/src/main.rs
+++ server_rust/src/main.rs
@@ -1,15 +1,16 @@
 mod database;
 mod network;
-
+mod utils;
 use database::{
     database_manager::DatabaseManager,
     migrations,
 };
 use network::server::Server;
-
+use utils::logger::logger::Logger;
+Logger::init();
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
+    
     let database_url =
         std::env::var("DATABASE_URL")?;
 
