ecab55bfc4cc8e61e200d0568676443921997ebb
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-29 08:29:22 +0900

    Enhance log function with context information
    Refactor log function to include additional context such as file name and line number.

--- client_python/logs.py
+++ client_python/logs.py
@@ -1,8 +1,20 @@
+import inspect
 import json
-def log(level,module,message):
-    payload = json.dumps({
-    "level": level,
-    "module": module,
-    "message": message
-}).encode("utf-8")
-    
+
+def log(client, level, message):
+    frame = inspect.currentframe().f_back
+
+    payload = {
+        "level": level,
+        "module": frame.f_globals["__name__"],
+        "file": frame.f_code.co_filename,
+        "line": frame.f_lineno,
+        "message": message,
+    }
+
+    client.send_packet(
+        Packet(
+            PacketType.LOG,
+            json.dumps(payload).encode("utf-8"),
+        )
+    )
