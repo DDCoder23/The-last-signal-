5d99a5102854366be10a97d39fcaa32644b7f65d
DDCoder23 <c.yuuto.c@gmail.com>
2026-07-30 21:04:23 +0900

    Replace println with debug for migration logging

--- server_rust/src/database/migrations.rs
+++ server_rust/src/database/migrations.rs
@@ -1,12 +1,12 @@
 use sqlx::PgPool;
-
+use log::debug;
 /// Exécute toutes les migrations SQL non encore appliquées.
 pub async fn run(pool: &PgPool) -> Result<(), sqlx::Error> {
     sqlx::migrate!("./migrations")
         .run(pool)
         .await?;
 
-    println!("Migrations PostgreSQL appliquées.");
+    debug!("Migrations PostgreSQL appliquées.");
 
     Ok(())
 }
