4dc84303079c776ea9ccbf5374b1a7d28a601698
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-01 22:06:46 +0900

    Update 0008_create_ban_sursis.sql

--- server_rust/migrations/0008_create_ban_sursis.sql
+++ server_rust/migrations/0008_create_ban_sursis.sql
@@ -10,10 +10,11 @@ CREATE TABLE IF NOT EXISTS banssursis (
 
     date_ban TIMESTAMP NOT NULL DEFAULT NOW(),
     sursis TIMESTAMP NOT NULL
-    
-
-    
-
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+    
+
 
     
