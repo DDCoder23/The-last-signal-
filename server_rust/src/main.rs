d0132932b52b4ce7c0fca6b15fb63536b0892a2f
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-08 22:15:44 +0900

    Add DATABASE_URL to DatabaseManager initialization

--- server_rust/src/main.rs
+++ server_rust/src/main.rs
@@ -11,11 +11,13 @@ use the_last_signal_server::utils::logger::logger::ServerLogger;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _guard = ServerLogger::init();
+    let database_url = 
+        std::env::var("DATABASE_URL")?;
     let database_path =
         std::env::var("DATABASE_PATH")?;
 
     let database =
-        DatabaseManager::new(&database_path)
+        DatabaseManager::new(&database_path,& database_url)
             .await?;
 
     database.ping().await?;
