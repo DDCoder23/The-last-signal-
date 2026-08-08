6e406056f16b75c16abf8b063b055e1fa046b6ef
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-08 16:25:28 +0900

    Add UNIQUE constraint to perms table

--- server_rust/migrations/0009_create_perms.sql
+++ server_rust/migrations/0009_create_perms.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS perms (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    perm TEXT NOT NULL
+    perm TEXT NOT NULL,
+    UNIQUE (name, perm)
 );
 
-INSERT INTO perms (name, perm)
+INSERT OR IGNORE INTO perms (name, perm)
 VALUES
     ('Cyril', 'admin'),
     ('Morgan', 'Super admin');
