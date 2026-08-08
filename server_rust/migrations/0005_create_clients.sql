d6a2c0050490b437bc9b99ccde37a473f9ff25de
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-08 12:05:02 +0900

    Refactor clients table schema in migration

--- server_rust/migrations/0005_create_clients.sql
+++ server_rust/migrations/0005_create_clients.sql
@@ -1,25 +1,16 @@
 CREATE TABLE IF NOT EXISTS clients (
-
-    client_id BIGSERIAL,
-
-    user_id UUID PRIMARY KEY,
+    client_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT,
 
     platform TEXT,
-
     game_version TEXT,
-
     os TEXT,
-
     cpu TEXT,
-
     gpu TEXT,
-
-    first_seen TIMESTAMP NOT NULL DEFAULT NOW(),
-
-    last_seen TIMESTAMP,
+    first_seen TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_seen TEXT,
 
     FOREIGN KEY (user_id)
         REFERENCES users(user_id)
         ON DELETE SET NULL
-
 );
