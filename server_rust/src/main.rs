96038d9300a49bcf73df8230132e99e235398610
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-08 18:18:02 +0900

    Change database URL to database path in main.rs

--- server_rust/src/main.rs
+++ server_rust/src/main.rs
@@ -11,11 +11,11 @@ use the_last_signal_server::utils::logger::logger::ServerLogger;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _guard = ServerLogger::init();
-    let database_url =
-        std::env::var("DATABASE_URL")?;
+    let database_path =
+        std::env::var("DATABASE_PATH")?;
 
     let database =
-        DatabaseManager::new(&database_url)
+        DatabaseManager::new(&database_path)
             .await?;
 
     database.ping().await?;
