d405afbd9cc776bcf4bf6ac6c4ccc2e8e8c0df30
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-12 18:46:42 +0900

    Make account_name unique in accounts table

--- server_rust/migrations/0002_create_accounts.sql
+++ server_rust/migrations/0002_create_accounts.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS accounts (
     account_id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id TEXT NOT NULL,
-    account_name TEXT NOT NULL,
+    account_name TEXT NOT NULL UNIQUE,
     created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (user_id, account_name)
+    
     FOREIGN KEY (user_id)
         REFERENCES users(user_id)
         ON DELETE CASCADE
