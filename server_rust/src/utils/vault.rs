59218d9ae044358e6dbe3dfa72c524200a67594c
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-12 13:56:01 +0900

    Refactor decrypt_vault to use environment variable for key

--- server_rust/src/utils/vault.rs
+++ server_rust/src/utils/vault.rs
@@ -3,22 +3,18 @@ use serde_json::Value;
 use std::fs;
 
 pub fn decrypt_vault() -> Result<Value, Box<dyn std::error::Error>> {
-    const KEY_FILE: &str = "./security/master.key";
-    const VAULT_FILE: &str = "../security/vault.enc";
-
-    // Lire la clé Fernet
-    let key = fs::read_to_string(KEY_FILE)?;
+    let key = match std::env::var("VAULT_KEY") {
+        Ok(key) => key,
+        Err(_) => fs::read_to_string("./security/master.key")?,
+    };
 
     let cipher = Fernet::new(key.trim())
         .ok_or("Clé Fernet invalide")?;
 
-    // vault.enc est un token Fernet texte
-    let encrypted = fs::read_to_string(VAULT_FILE)?;
+    let encrypted = fs::read_to_string("./security/vault.enc")?;
 
-    // fernet 0.2.2 retourne directement un Result
     let decrypted = cipher.decrypt(encrypted.trim())?;
 
-    // JSON déchiffré
     let vault: Value = serde_json::from_slice(&decrypted)?;
 
     Ok(vault)
