3d9c822081a4adcd0865202eca4d84de7da86f02
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-12 17:21:06 +0900

    Update file paths for vault decryption

--- server_rust/src/utils/vault.rs
+++ server_rust/src/utils/vault.rs
@@ -5,13 +5,13 @@ use std::fs;
 pub fn decrypt_vault() -> Result<Value, Box<dyn std::error::Error>> {
     let key = match std::env::var("VAULT_KEY") {
         Ok(key) => key,
-        Err(_) => fs::read_to_string("./security/master.key")?,
+        Err(_) => fs::read_to_string("../security/master.key")?,
     };
 
     let cipher = Fernet::new(key.trim())
         .ok_or("Clé Fernet invalide")?;
 
-    let encrypted = fs::read_to_string("./security/vault.enc")?;
+    let encrypted = fs::read_to_string("../security/vault.enc")?;
 
     let decrypted = cipher.decrypt(encrypted.trim())?;
 
