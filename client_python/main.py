6753b3200a3c975f1b9113fa53de3935e6abaed0
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-13 15:54:31 +0900

    Refactor packet reception with specific types
    Updated packet reception to specify packet types for login and chat messages.

--- client_python/main.py
+++ client_python/main.py
@@ -11,28 +11,30 @@ def main():
     time.sleep(1)
     client2.connect()
     client.send_packet(LoginPacket("Momo"))
-    response = client.receive_packet()
-    print(type(response))
-    print(response)
-    print(response.packet_type)
+    response = client.receive_packet(
+    PacketType.LOGIN
+    )
+    
+    print(reponse.username)
+    
     
     client.send_packet(
         ChatPacket("Momo dirige le jeu")
   )
-    response = client.receive_packet()
+    response = client.receive_packet(PacketType.CHAT)
     client.send_packet(
         ChatPacket("Le serveur est en rust")
   )
-    response = client.receive_packet()
+    response = client.receive_packet(PacketType.CHAT)
     print(response.message)
     client.send_packet(
         ChatPacket("Le client est en python")
   )
-    response = client.receive_packet()
+    response = client.receive_packet(PacketType.CHAT)
     print(response.message)
     client.send_packet(
         ChatPacket("Tests des trésors en cours...")
   )
-    response = client.receive_packet()
+    response = client.receive_packet(PacketType.CHAT)
     print(response.message)
     client.disconnect()
