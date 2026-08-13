e826fb3efc6e0a7d2f0826ed7994be07c7a5b4b7
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-13 20:03:56 +0900

    Replace login test with tresor opening tests

--- server_rust/tests/integration_test.rs
+++ server_rust/tests/integration_test.rs
@@ -1,8 +1,31 @@
 use the_last_signal_server::network::packet::PacketType;
 
 #[test]
-fn test_packet_login() {
-    assert_eq!(PacketType::Login as u8, 2);
+fn test_tresor() {
+    let mut tresor = Tresor::new();
+    for i in 1..=1000{
+    for i in 1..=7 {
+        
+
+    let message_normal = tresor
+        .ouvrir(
+                  &database.pool().clone(),
+            1,
+            i, 
+            false)
+        .await?;
+    debug!("Trésor {} normal : {:?}", i, message_normal);
+
+    let message_admin = tresor
+        .ouvrir(
+                &database.pool().clone(),
+                1,
+               i,
+               true)
+        .await?;
+    debug!("Trésor {} admin : {:?}", i, message_admin);
+    }
+    }
 }
 use serde_json::{Map, Value};
 
