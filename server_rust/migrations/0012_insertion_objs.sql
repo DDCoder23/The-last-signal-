20501cb8174f47a86bbc75573246614a25f262d3
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-14 23:20:21 +0900

    Add multiple items to objets_dispo table
    Inserted multiple new items into the objets_dispo table with their respective types, image paths, and prices.

---
+++ server_rust/migrations/0012_insertion_objs.sql
@@ -0,0 +1,32 @@
+INSERT INTO objets_dispo (
+    nom,
+    type,
+    image_path,
+    prix_vente
+)
+VALUES
+    ('épée de bois', 'armes', 'epee_bois.png', 20),
+
+    ('livre enchant niv 1', 'enchanted_book', 'livre enchant.png', 100000),
+    ('livre enchant niv 2', 'enchanted_book', 'livre enchant.png', 200000),
+    ('livre enchant niv 3', 'enchanted_book', 'livre enchant.png', 300000),
+    ('livre enchant niv 4', 'enchanted_book', 'livre enchant.png', 400000),
+    ('livre enchant niv 5', 'enchanted_book', 'livre enchant.png', 500000),
+    ('livre enchant niv 6', 'enchanted_book', 'livre enchant.png', 600000),
+
+    ('viande', 'consumable', 'viande.png', 7),
+    ('pain', 'consumable', 'pain.png', 5),
+
+    ('potion de soin léger', 'potion', 'potion_de_soin_léger.png', 25),
+    ('potion de soin modéré', 'potion', 'potion_de_soin_modéré.png', 35),
+    ('potion de délivrance', 'potion', 'potion_de_délivrance.png', 100),
+    ('potion de mana', 'potion', 'potion_de_mana.png', 75),
+
+    ('gemmes', 'basic', 'gemmes.png', 66667),
+    ('sac', 'material', 'sac.png', 5),
+    ('corde', 'material', 'corde.png', 5),
+
+    ('chargeur', 'equipment', 'chargeur.png', 120),
+    ('Carreau d'' arbalète', 'equipment', 'carreau d'' arbalète.png', 15),
+
+    ('torche', 'material', 'torche.png', 5);
