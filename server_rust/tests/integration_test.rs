8b6c28188746cf067de9104529af5d6164b046db
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-14 10:12:57 +0900

    Make test_tresor async and initialize database
    Refactor test_tresor to be asynchronous and add database initialization.

--- server_rust/tests/integration_test.rs
+++ server_rust/tests/integration_test.rs
@@ -1,7 +1,35 @@
 
+use the_last_signal_server::network::packet::PacketType;
+use the_last_signal_server::database::{
+    database_manager::DatabaseManager,
+    migrations,
+};
+use log::{debug,info};
+
+use the_last_signal_server::utils::logger::logger::ServerLogger;
+
 use the_last_signal_server::gameplay::tresor::Tresor;
 #[test]
-fn test_tresor() {
+async fn test_tresor() {
+    let _guard = ServerLogger::init();
+    
+    
+    let database_url = 
+        std::env::var("DATABASE_Test_URL")?;
+    let database_path =
+        std::env::var("DATABASE_Test_PATH")?;
+
+    let database =
+        DatabaseManager::new(&database_path,& database_url)
+            .await?;
+
+    database.ping().await?;
+
+    migrations::run(&database.pool())
+        .await?;
+
+    info!("Base SQLite prête.");
+    ServerLogger::set_database(database.pool().clone());
     let mut tresor = Tresor::new();
     for i in 1..=10000{
     for i in 1..=7 {
