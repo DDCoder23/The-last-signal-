ff95f1cd259d357a6d3159fdbed842a1bc13206d
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-16 19:28:19 +0900

    Add test for communication key creation
    Added a new test for communication key generation.

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,4 +2,8 @@ from client_python.main import main
 
 def test_main():
     main()
-
+def test_key():
+    key1 = get_or_create_communication_key()
+    print(len(key1))
+    key2 = get_or_create_communication_key()
+    print(key1 == key2)
