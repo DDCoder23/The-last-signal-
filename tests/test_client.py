a2c0cde51d26943f32d5eaa392c31a12831fc7da
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-16 19:29:19 +0900

    Update key retrieval to use vault
    Refactor key retrieval to use vault module.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,9 +1,9 @@
 from client_python.main import main
-
+from security import vault
 def test_main():
     main()
 def test_key():
-    key1 = get_or_create_communication_key()
+    key1 = vault.get_or_create_communication_key()
     print(len(key1))
-    key2 = get_or_create_communication_key()
+    key2 = vault.get_or_create_communication_key()
     print(key1 == key2)
