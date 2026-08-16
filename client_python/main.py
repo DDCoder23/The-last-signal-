e06919e6808fcc8d3b6aa08074e5315816d09149
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-16 19:24:31 +0900

    Import vault from security module

--- client_python/main.py
+++ client_python/main.py
@@ -5,6 +5,7 @@ from .packets.chat import ChatPacket
 from .packets.login import LoginPacket
 from .packets.ping import PingPacket
 from .packets.move import MovePacket
+from security import vault
 def main():
     client = Client()
     client2 = Client()
