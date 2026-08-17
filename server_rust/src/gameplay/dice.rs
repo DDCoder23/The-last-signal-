33da38f81dfaac098d341ace25d92d0cab388e78
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-17 21:42:59 +0900

    Fix RNG initialization and method in jet_de_des

--- server_rust/src/gameplay/dice.rs
+++ server_rust/src/gameplay/dice.rs
@@ -1,9 +1,9 @@
 use rand::Rng;
 
 pub fn jet_de_des(face: u32, nb: u32) -> u32 {
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
 
     (0..nb)
-        .map(|_| rng.gen_range(1..=face))
+        .map(|_| rng.random_range(1..=face))
         .sum()
 }
