2b2f6fb13796028137000bc76306e3dbd7ed1daf
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-20 21:38:06 +0900

    Add signup payload parsing to handler

--- server_rust/src/network/handler.rs
+++ server_rust/src/network/handler.rs
@@ -3,7 +3,7 @@ use crate::network::packet::{
     PacketType,LogLevel,ClientLog,
 };
 use crate::network::client::Client;
-use crate:: network::parser::parse_login_payload;
+use crate:: network::parser::{parse_login_payload,parse_signup_playload};
 use log::{trace, debug, info, warn, error};
 pub struct PacketHandler;
 use crate::utils::password::{verify_password,hash_password};
