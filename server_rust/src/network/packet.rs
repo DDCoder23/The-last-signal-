1e071e4f3e62b7d473944eee5c53c82e7ef66e50
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-20 17:23:39 +0900

    Add Signup packet type to PacketType enum

--- server_rust/src/network/packet.rs
+++ server_rust/src/network/packet.rs
@@ -20,6 +20,7 @@ pub enum PacketType {
     Chat = 3,
     Move = 4,
     Log = 5,
+    Singup = 6,
 }
 
 
@@ -49,6 +50,7 @@ impl PacketType {
             3 => Some(PacketType::Chat),
             4 => Some(PacketType::Move),
             5 => Some(PacketType::Log),
+            6 => Some(PacketType::Singup),
             _ => None,
         }
     }
