5be2f4168724436419ccc1030ffffdc4c8384e32
DDCoder23 <c.yuuto.c@gmail.com>
2026-08-20 17:09:05 +0900

    Add SingupPacket and LogPacket imports

--- client_python/packets/__init__.py
+++ client_python/packets/__init__.py
@@ -1,4 +1,6 @@
 from .ping import PingPacket
 from .login import LoginPacket
+from .singup import SingupPacket
+from .log import LogPacket
 from .chat import ChatPacket
 from .move import MovePacket
