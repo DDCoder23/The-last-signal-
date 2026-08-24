fb9619b903565d49642c1e81c4dce74f2334e11c
DDCoder23 <252053530+DDCoder@users.noreply.github.com>
2026-08-24 14:55:23 +0200

    correction erreur python

--- client_python/client.py
+++ client_python/client.py
@@ -99,7 +99,7 @@ class Client:
                 if data is None:
                     return None
                 packet = Packet.decode(data)
-                if packet.packet_type == PacketType.BAN:
+                if packet.packet_type == packet_type.BAN:
                     self.disconnect()
                     return packet
                 if packet.packet_type == expected_type:
