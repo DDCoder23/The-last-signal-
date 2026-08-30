430e16f45b9c5daeeb853304d1b605afb2ea4226
DDCoder23 <252053530+DDCoder23@users.noreply.github.com>
2026-08-30 22:01:17 +0200

    Update test_client.py

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,13 @@
 from client_python.main import main
 from security import vault
+import pytest
+
 def test_main():
-    main()
+    with pytest.raises(SystemExit) as exc:
+        main()
+
+    assert exc.value.code == 1
+    
 def test_key():
     key1 = vault.get_or_create_communication_key()
     print(len(key1))
