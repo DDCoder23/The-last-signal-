ab0f4cd952178f82eb0f1c638bd2825bec015e2b
DDCoder23 <DDCoder23@users.noreply.github.com>
2026-09-04 21:40:07 +0200

    refactor logic of notation

--- scripts/docs_score.py
+++ scripts/docs_score.py
@@ -2,4 +2,6 @@ from documentation.score import generate_score
 
 if __name__ == "__main__":
     score = generate_score()
+    if score < 0:
+        score = 0
     print(f"Documentation score : {score}/100")
