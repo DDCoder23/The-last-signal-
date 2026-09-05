2889fc32c1a23a92576d90bba2979278d1bcb398
DDCoder23 <252053530+DDCoder@users.noreply.github.com>
2026-09-05 08:45:37 +0200

    amélioration

--- scripts/docs_score.py
+++ scripts/docs_score.py
@@ -2,6 +2,5 @@ from documentation.score import generate_score
 
 if __name__ == "__main__":
     score = generate_score()
-    if score < 0:
-        score = 0
+    
     print(f"Documentation score : {score}/100")
