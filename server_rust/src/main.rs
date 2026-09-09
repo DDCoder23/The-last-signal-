efd7549c151f557b069e98cfef85243c7575fe0d
DDCoder23 <252053530+DDCoder23@users.noreply.github.com>
2026-09-09 21:00:06 +0200

    Add inventory management with conversion functionality

--- server_rust/src/main.rs
+++ server_rust/src/main.rs
@@ -4,8 +4,9 @@ use the_last_signal_server::database::{
 };
 use log::info;
 use the_last_signal_server::network::server::Server;
+use the_last_signal_server::gameplay::{objets::Livre,table_de_conversion::convertir_livres};
 use the_last_signal_server::utils::logger::logger::ServerLogger;
-
+use std::collections::HashMap;
 #[tokio::main]
 
 /*
@@ -38,8 +39,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     info!("Base SQLite prête.");
     ServerLogger::set_database(database.pool().clone());
+    let mut inventaire = HashMap::new();
+    inventaire.insert("livre enchant niv 1".to_string(), 
+                      Livre::new(nom: String::from("livre enchant niv 1"), 
+                                 quantite: 1, 
+                                 category: String::from("épée") , 
+                                 enchantements: Vec::from["Aura de feu I"], 
+                                niv: 1,)),
+    inventaire.insert("livre enchant niv 1".to_string(), 
+                      Livre::new(nom: String::from("livre enchant niv 1"), 
+                                 quantite: 1, 
+                                 category: String::from("épée") , 
+                                 enchantements: Vec::from["Aura de feu I"], 
+                                niv: 1,)),
+    convertir_livres(
+        stuff: inventaire,
+        niv: 2,
+        nb: 1,).await?;
     
-
     let server =
         Server::new(
             "127.0.0.1:5000",
